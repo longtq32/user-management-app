566bddc7fb132b7b71c4ac8ec0a5070aff15a7a5
DESKTOP-VJVQCAV\damha <longtq32@gmail.com>
2024-03-26 23:21:08 +0700

    modify input for beautiful

--- src/Form.jsx
+++ src/Form.jsx
@@ -24,22 +24,57 @@ function Form({ addData }) {
   };
 
   const handleChange = (e) => {
-    const { name, value} = e.target;
-    setFormData( (prevData) => ({
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
-
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="id" placeholder="ID" value={formData.id} onChange={handleChange}/>
-      <input type="text" name="name" placeholder="name" value={formData.name} onChange={handleChange}/>
-      <input type="text" name="gender" placeholder="gender" value={formData.gender} onChange={handleChange}/>
-      <input type="text" name="year_of_birth" placeholder="year_of_birth" value={formData.year_of_birth} onChange={handleChange}/>
-      <input type="text" name="province" placeholder="province" value={formData.province} onChange={handleChange}/>
-      <input type="text" name="position" placeholder="position" value={formData.position} onChange={handleChange}/>
+      <input
+        type="text"
+        name="id"
+        placeholder="ID"
+        value={formData.id}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="name"
+        placeholder="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="gender"
+        placeholder="gender"
+        value={formData.gender}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="year_of_birth"
+        placeholder="year_of_birth"
+        value={formData.year_of_birth}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="province"
+        placeholder="province"
+        value={formData.province}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="position"
+        placeholder="position"
+        value={formData.position}
+        onChange={handleChange}
+      />
       <button type="submit">Thêm</button>
     </form>
   );
